df16cc564ed44c0979ab23b9d4c4c651d302851d
seppzero <seppzero@gmail.com>
2022-01-31 15:32:20 +0100

    test

--- stories/Button.jsx
+++ stories/Button.jsx
@@ -10,11 +10,13 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}
+      {label} asd
     </button>
   );
 };
